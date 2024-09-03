0cecc8edfc37e7a5c44b730c0088ae86dcbd7014
Aurelien St <aurelien.strimelle@bstorm.be>
2024-09-03 10:59:56 +0200

    Mise en place du DBContext 🏗

---
+++ Demo_EF/Database/DemoDbContext.cs
@@ -0,0 +1,21 @@
+using Demo_EF.Database.Configurations;
+using Demo_EF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo_EF.Database
+{
+    public class DemoDbContext : DbContext
+    {
+        #region Entités de la DB
+        public DbSet<Car> Car { get; set; }
+        #endregion
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Définition de la connection string
+            //  -> Setup pour un utilisation simple via l'opérateur "new"
+            //  https://learn.microsoft.com/en-us/ef/core/dbcontext-configuration/
+            optionsBuilder.UseSqlServer("Server=localhost;Database=Demo_EFCore;Trusted_Connection=True;Trust Server Certificate=True");
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -5,3 +5,6 @@
 - Microsoft.EntityFrameworkCore.SqlServer
 - Microsoft.EntityFrameworkCore.Tools
 - Microsoft.EntityFrameworkCore.Design
+
+## Mise en place de la classe � DbContext �
+https://learn.microsoft.com/en-us/ef/core/dbcontext-configuration/
