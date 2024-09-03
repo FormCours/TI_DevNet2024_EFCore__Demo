48b8b109bf104ea00d0eea2002b8ee9807167725
Aurelien St <aurelien.strimelle@bstorm.be>
2024-09-03 10:52:50 +0200

    Creation du modèle "Car"  🚘

---
+++ Demo_EF/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿namespace Demo_EF.Models
+{
+    public class Car
+    {
+        public enum StateEnum {
+            NEW,
+            OCCASION,
+            FOR_PARTS
+        }
+
+        public int Id { get; set; }
+        // TODO Add Brand with relation
+        public required string Model { get; set; }
+        public decimal Price { get; set; }
+        public DateTime? RegistrationDate { get; set; }
+        public StateEnum State { get; set; }
+    }
+}
